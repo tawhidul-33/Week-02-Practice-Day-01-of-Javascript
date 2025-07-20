1248506a5add22ea7ba6d1f7b7d89e5a6d5d0849
tawhidul-33 <tawhidul2442@gmail.com>
2025-07-20 17:31:41 +0600

    Delete Module-2.5 Week 02 Practice Day 01 directory

--- Module-2.5 Week 02 Practice Day 01/2_No_Even-or-odd.js
+++
@@ -1,9 +0,0 @@
-function evenOrodd(inputNumber) {
-    if (inputNumber % 2 == 0) {
-        console.log("Even");
-    } else {
-        console.log("Odd");
-    }
-};
-evenOrodd(6) //Input number
-
--- Module-2.5 Week 02 Practice Day 01/3_No_Sort.js
+++
@@ -1,3 +0,0 @@
-const Array=[2,4,6,8,10,20,18,16,14,12,1,3,5,7,9,19,17,15,13,11];
-Array.sort((a,b)=>a-b);//sorted with ASC
-console.log(Array);
